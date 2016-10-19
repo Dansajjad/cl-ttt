6e6d5ecf7573a60441798eee1c923ef9ab7b0643
Dan Sajjad <dan.sajjad@gmail.com>
2016-10-18 22:45:37 -0700

    Created print board function, fixed misc. errors

--- ttt.js
+++ ttt.js
@@ -1,12 +1,19 @@
-const prompt = require('prompt');
+// const prompt = require('prompt');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 
-let boardContent, isOccupied, winCombo, turn;
+let boardContent, isOccupied, numSquaresFilled, winCombo, turn, status;
 
-function initialize() {
+(function initialize() {
     turn = 0;  
     isOccupied = []; //
     boardContent = []; //
+    numSquaresFilled = 0;
     winCombo = [
       [0, 1, 2],
       [3, 4, 5],
@@ -21,9 +28,22 @@ function initialize() {
     //looping to...
     for (var i = 0; i < 9; i++) {
       isOccupied[i] = false;
-      boardContent[i] = "";
+      boardContent[i] = i;
     }
-}();
+    printBoard();
+
+    rl.question('Please select a cell Num? ', (answer) => {
+      // TODO: Log the answer in a database
+      
+      rl.on('line', (answer) => {
+        console.log('You have selected cell #:', answer);
+        markCell(answer); 
+      });
+
+      if(status === "game over") rl.close();
+    });
+
+}());
 
 function markCell (cellNum) {
   //check if cell is occupied
@@ -39,24 +59,38 @@ function markCell (cellNum) {
     turn++;
     numSquaresFilled++;
     isOccupied[cellNum] = true;
-    checkforWinners(boardContent[cellNum]);
-    if(numSquaresFilled === 9) console.log("Game Over");
+    console.log('checking for winner');
+    checkForWinners(boardContent[cellNum]);
+    if(numSquaresFilled === 9) {
+      console.log("Game Over");
+      status = "game over";
+      rl.close();
+    }
+    
+    printBoard();
+
   } else {
-    return "Select another cell";
+    console.log("The cell is occupied, please select another cell");
   }
 }
 
 function checkForWinners(symbol) {
+  console.log('HELLO', symbol);
   for(let i = 0; i < winCombo.length; i++) {
-    if(boardContent[i][0] == symbol &&
-       boardContent[i][1] == symbol &&
-       boardContent[i][2] == symbol) {
+    if(winCombo[i][0] == symbol &&
+       winCombo[i][1] == symbol &&
+       winCombo[i][2] == symbol) {
         console.log(symbol + "Won!");
+        status = "game over";
     }
   }
 }
 
 
 function printBoard () {
-
+  console.log("\n" +
+    boardContent[0] + "|" + boardContent[1] + "|" +  boardContent[2] + "\n" +        
+    boardContent[3] + "|" + boardContent[4] + "|" +  boardContent[5] + "\n" +        
+    boardContent[6] + "|" + boardContent[7] + "|" +  boardContent[8] + "\n"        
+              );
 }
